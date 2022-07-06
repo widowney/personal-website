30f89c87cfad92fd23ada3fd006a9054413a6899
Will Downey <willd20@vt.edu>
2022-07-06 17:12:39 -0400

    Install particle components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import Particles from "react-tsparticles";
+import { loadFireworksPreset } from "tsparticles-preset-fireworks";
+import Header from "./components/Header";
 
-function App() {
+
+const App = () => {
+  const fireworksInit = async (main) => {
+    await loadFireworksPreset(main);
+  };
+
+  const particlesLoaded = (container) => {
+    console.log(container);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <Particles
+        id="tsparticles"
+        init={fireworksInit}
+        loaded={particlesLoaded}
+        options={{
+          preset: "fireworks",
+        }}
+      />
     </div>
+
   );
-}
+};
 
 export default App;
