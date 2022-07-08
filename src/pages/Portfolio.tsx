e941f66f8e13190421e9e317d5fb5b0765ae1092
Will Downey <willd20@vt.edu>
2022-07-08 19:39:08 -0400

    Dark mode title color

--- src/pages/Portfolio.tsx
+++ src/pages/Portfolio.tsx
@@ -6,7 +6,7 @@ const Portfolio = () => {
     return (
         <div className="min-h-screen flex flex-col items-center sm:mr-16 sm:ml-16" id="Portfolio">
             <span className="text-transparent">.</span>
-            <h1 className="text-3xl text-center mb-4 border-b-2 w-1/2 mt-10">Portfolio</h1>
+            <h1 className="text-3xl text-center mb-4 border-b-2 w-1/2 mt-10 dark:text-white font-bold">Portfolio</h1>
             <div className="flex w-full gap-2 flex-shrink-0 flex-wrap justify-center align-center">
                 <div className="w-80">
                     <Card imgSrc="https://flowbite.com/docs/images/blog/image-1.jpg">
