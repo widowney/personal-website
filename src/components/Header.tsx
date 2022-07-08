13b6ce10ca327bd7f64888303c9a5d88026a1853
Will Downey <willd20@vt.edu>
2022-07-08 17:42:15 -0400

    Header is fixed to the top

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,39 +5,42 @@ import WD from '../images/WD.svg';
 const Header: React.FunctionComponent = () => {
     console.log(WD);
     return (
-        <Navbar
+        <div className="fixed w-full z-10">
+            <Navbar
             fluid={true}
             rounded={true}
             >
-            <Navbar.Brand href="#">
-                <img
-                src={WD}
-                className="mr-3 h-8 sm:h-12"
-                alt="Flowbite Logo"
-                />
-            </Navbar.Brand>
-            <Navbar.Toggle />
-            <Navbar.Collapse>
-                <Navbar.Link
-                href="/navbars"
-                active={true}
-                >
-                Home
-                </Navbar.Link>
-                <Navbar.Link href="/navbars">
-                About
-                </Navbar.Link>
-                <Navbar.Link href="/navbars">
-                Portfolio
-                </Navbar.Link>
-                <Navbar.Link href="/navbars">
-                Resume
-                </Navbar.Link>
-                <Navbar.Link href="/navbars">
-                Contact
-                </Navbar.Link>
-            </Navbar.Collapse>
-        </Navbar>
+                <Navbar.Brand href="#">
+                    <img
+                    src={WD}
+                    className="mr-3 h-8 sm:h-12"
+                    alt="Flowbite Logo"
+                    />
+                </Navbar.Brand>
+                <Navbar.Toggle />
+                <Navbar.Collapse>
+                    <Navbar.Link
+                    href="/navbars"
+                    active={true}
+                    >
+                    Home
+                    </Navbar.Link>
+                    <Navbar.Link href="#About">
+                    About
+                    </Navbar.Link>
+                    <Navbar.Link href=".Portfolio">
+                    Portfolio
+                    </Navbar.Link>
+                    <Navbar.Link href="#Resume">
+                    Resume
+                    </Navbar.Link>
+                    <Navbar.Link href=".Contact">
+                    Contact
+                    </Navbar.Link>
+                </Navbar.Collapse>
+            </Navbar>
+        </div>
+        
     );
 }
 
