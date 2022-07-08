eb35206c99088aec4ed934f1391b7ba523a1614c
Will Downey <willd20@vt.edu>
2022-07-08 11:27:26 -0400

    Add particles container to background

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from './components/Header';
 import ThemeBtn from './components/ThemeBtn';
+import ParticlesContainer from './components/Particles';
 
 function App() {
   return (
     <div className="App h-full">
       <Header />
       <ThemeBtn />
+      <ParticlesContainer />
     </div>
   );
 }
