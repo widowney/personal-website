c0dcab0b72e3f5bccbcd7804f0844ddc143c7613
Will Downey <willd20@vt.edu>
2022-07-09 15:41:04 -0400

    Footer content

---
+++ src/components/Footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const BuildFooter = () => {
+    return (
+        <div className="flex justify-center dark:text-white text-lg font-bold p-4">
+            Designed by: Will Downey
+        </div>
+        
+
+    );
+}
+
+export default BuildFooter;
